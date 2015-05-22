940ae527ff65d448ea9ca65a82cdff9c9b9e5bfe
Seigo Uchida <spesnova@gmail.com>
2015-05-22 21:12:37 +0900

    Implements simple http server

---
+++ irukad/iruka.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "hello")
+	})
+
+	// Middleware stack
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+	)
+
+	n.UseHandler(r)
+
+	n.Run(":3000")
+}
