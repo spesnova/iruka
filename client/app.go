4cc867fced72fba121d4d775bef3357f19ef7f83
Seigo Uchida <spesnova@gmail.com>
2015-05-28 16:49:33 +0900

    client: Implements AppUpdate

--- client/app.go
+++ client/app.go
@@ -17,3 +17,8 @@ func (c *Client) AppList() ([]schema.App, error) {
 	var appsRes []schema.App
 	return appsRes, c.Get(&appsRes, "/apps")
 }
+
+func (c *Client) AppUpdate(appIdentity string, opts schema.AppUpdateOpts) (schema.App, error) {
+	var appRes schema.App
+	return appRes, c.Update(&appRes, "/apps/"+appIdentity, opts)
+}
--- client/iruka.go
+++ client/iruka.go
@@ -74,3 +74,26 @@ func (c *Client) Get(v interface{}, path string) error {
 	err = json.NewDecoder(res.Body).Decode(&v)
 	return err
 }
+
+func (c *Client) Update(v interface{}, path string, opts interface{}) error {
+	j, err := json.Marshal(opts)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PATCH", c.URL+path, bytes.NewReader(j))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&v)
+	return err
+}
