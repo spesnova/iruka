c334dfc45790450f7c9353a91edee3a98002270f
Seigo Uchida <spesnova@gmail.com>
2015-05-28 15:39:02 +0900

    cli: Create CLI prototype

---
+++ iruka/commands.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+)
+
+var Commands = []cli.Command{
+	cmdApps,
+}
--- iruka/iruka.go
+++ iruka/iruka.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/codegangsta/cli"
+
+	"github.com/spesnova/iruka/client"
+)
+
+var (
+	client *iruka.Client
 )
 
 func main() {
 	app := cli.NewApp()
+	app.Name = "iruka"
+	app.Usage = "iruka CLI"
+	app.Version = "0.1.0"
+	app.Author = "Seigo Uchida"
+	app.Email = "spesnova@gmail.com"
+	app.Commands = Commands
+
+	client = iruka.NewClient()
+
+	app.Run(os.Args)
 }
