07c7d0af06d060abc14e3477a1ea8791442a23f8
Daisuke Fujita <dtanshi45@gmail.com>
2015-06-16 15:47:01 +0900

    Implement Config Vars set command

--- iruka/config.go
+++ iruka/config.go
@@ -16,6 +16,7 @@ var cmdConfig = cli.Command{
 	Usage: "manage config vars for an app(set, show, unset)",
 	Subcommands: []cli.Command{
 		cmdConfigShow,
+		cmdConfigSet,
 	},
 }
 
@@ -34,6 +35,23 @@ EXAMPLE:
 	Action: runConfigShow,
 }
 
+var cmdConfigSet = cli.Command{
+	Name:  "set",
+	Usage: "Set config vars to the app",
+	Description: `
+   $ iruka config set <APP> <KEY1>=<VALUE1>,<KEY2>=<VALUE2>
+
+EXAMPLE:
+
+   $ iruka config set example HELLO=world,SOME_FLAG=1
+   FOO:       bar
+   BAZ:       qux
+   HELLO:     world
+   SOME_FLAG: 1
+`,
+	Action: runConfigSet,
+}
+
 func runConfigShow(c *cli.Context) {
 	if len(c.Args()) != 1 {
 		cli.ShowCommandHelp(c, "show")
@@ -48,6 +66,35 @@ func runConfigShow(c *cli.Context) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	out = showConfigVars(out, res)
+	out.Flush()
+}
+
+func runConfigSet(c *cli.Context) {
+	if len(c.Args()) != 2 {
+		cli.ShowCommandHelp(c, "set")
+		os.Exit(1)
+	}
+
+	appIdentity := c.Args().First()
+
+	configVars := make(map[string]string)
+	keyValuesString := c.Args()[1]
+	keyValues := strings.Split(keyValuesString, ",")
+	for i := range keyValues {
+		keyValue := strings.Split(keyValues[i], "=")
+		configVars[keyValue[0]] = keyValue[1]
+	}
+
+	res, err := client.ConfigVarsUpdate(appIdentity, configVars)
+	if err != nil {
+		fmt.Println("error")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// TODO(dtan4): restart container here
 
 	out = showConfigVars(out, res)
 	out.Flush()
