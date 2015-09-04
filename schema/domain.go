64822f0775496b4a65a7cfe333439a0d33bd5ef5
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-04 19:49:01 +0900

    Reconstruct registry of domain

--- schema/domain.go
+++ schema/domain.go
@@ -8,6 +8,7 @@ import (
 
 type Domain struct {
 	ID        uuid.UUID `json:"id"`
+	AppID     uuid.UUID `json:"app_id"`
 	Hostname  string    `json:"hostname"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
